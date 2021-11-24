31c9c7b01117244ed8ee234b82432a6795c3d5e5
Mr Potato <75535434+MrPotato374@users.noreply.github.com>
2021-11-24 22:49:48 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="multipurpose-discord",
+    author="MrPotato",
+    url="",
+    version='0.0.1',
+    packages=['multipurposediscord'],
+    license='MIT',
+    description="A package made for multipurpose stuff!",
+    long_description_content_type="text/markdown",
+    long_description='Soon!',
+    install_requires=[],
+    python_requires='>=3.5.3',
+    include_package_data=True,
+    keywords=[
+        'discord.py', 
+        'discord', 
+        'discord-multipurpose'
+    ]
+)
